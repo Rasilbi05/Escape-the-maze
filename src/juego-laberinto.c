cc0e9a2f554d4507400c9988daa8265c2856737d
Rasilbi05 <97067133+Rasilbi05@users.noreply.github.com>
2026-05-28 11:29:15 +0200

    actualizar_posicion_jugador completa
    Se ha finalizado la función pendiente de actualizar la posición del jugador. Versión no definitiva. También se ha cambiado la matriz de visión del jugador como una matriz constante con sus valores representativos.

--- src/juego-laberinto.c
+++ src/juego-laberinto.c
@@ -13,16 +13,75 @@
 #include "juego-laberinto.h"
 
 void actualizar_posicion_jugador(uint16_t x, uint16_t y){
+	
+	//Quitamos las luces previas
+	for(uint16_t i = 0; i < TAM_X; ++i){
+		for(uint16_t j = 0; j < TAM_Y; ++j){
+			if(Laberinto[i][j] == LUZ){
+				Laberinto[i][j] = OSCURIDAD;
+			}else if(Laberinto[i][j] == JUGADOR){
+				Laberinto[i][j] = 0;
+			}
+		}
+	}
+	
 	//Actualizamos la posición del jugador
 	J1.x = x;
 	J1.y = y;
 	
 	//Si hay luz, actualizamos el cono de visión
 	if(/*funcion encargada de comprobar el lcd*/){
+		int8_t dir_x = 0, dir_y = 0, per_x = 0, per_y = 0;
 		
-	}else{//ponemos todo a oscuras
+		switch(J1.ultima_direccion){
+			case JOYSTICK_ARRIBA:
+				dir_x = -1; dir_y = 0; per_x = 0; per_y = 1;
+			break;
+			case JOYSTICK_ABAJO:
+				dir_x = 1; dir_y = 0; per_x = 0; per_y = -1;
+			break;
+			case JOYSTICK_DERECHA:
+				dir_x = 0; dir_y = 1; per_x = 1; per_y = 0;
+			break;
+			case JOYSTICK_IZQUIERDA:
+				dir_x = 0; dir_y = -1; per_x = -1; per_y = 0;
+			break;
+		}
 		
+		uint16_t centro = VISION/2;
+		//recorremos el cono de visión para actualizar el laberinto
+		for(uint16_t i = 0; i<VISION; ++i){
+			for(uint16_t j = 0; j<VISION;++j){
+				if(Vision[i][j] == 1){
+					//distancias respecto al centro
+					int16_t frente = centro - i;
+					int16_t lateral = j - centro;
+					
+					//calculamos las coordenadas con respecto al laberinto
+					int16_t laberinto_x = J1.x + (frente*dir_x) + (lateral*per_x);
+					int16_t laberinto_y = J1.y + (frente*dir_y) + (lateral*per_y);
+					
+					//actualizamos los valores en el laberinto
+					if(laberinto_x >= 0 && laberinto_x < TAM_X && laberinto_y >= 0 && laberinto_y < TAM_Y){
+						if(Laberinto[laberinto_x][laberinto_y] == 1){	//hay pared
+							Laberinto[laberinto_x][laberinto_y] = LUZ;
+						}
+					}
+				}
+			}
+		}	
 	}
+	
+	//marcamos las casillas del jugador
+	for(int16_t dx = -5; dx < 5; ++dx){
+		for(int16_t dy = -5; dy < 5; ++dy){
+			int16_t jug_x = J1.x + dx;
+			int16_t jug_y = J1.y + dy;
+			if(jug_x >= 0 && jug_x < TAM_X && jug_y >= 0 && jug_y < TAM_Y){
+					Laberinto[jug_x][jug_y] = JUGADOR;
+			}
+		}
+	}	
 }
 
 void laberinto_inicializar(){
@@ -42,7 +101,7 @@ void laberinto_inicializar(){
 				uint16_t correspondiente_y = j*10;
 				for(uint16_t c = correspondiente_x; c < correspondiente_x + 10; ++c){
 					for(uint16_t f = correspondiente_y; f < correspondiente_y + 10; ++f){
-						if(f < TAM_X && c < TAM_Y){
+						if(c < TAM_X && f < TAM_Y){
 							Laberinto[f][c] = 1;
 						}
 					}
